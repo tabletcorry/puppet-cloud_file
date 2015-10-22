b604c95c74d438daf97849597bbd42ec4c24a6f4
Corry Haines <github@corryh.com>
2015-10-22 09:16:22 -0700

    Use SDKv2
    Switch to new AWS SDK, required for accessing KMS (as KMS requires
    signature v4)

--- lib/puppet/provider/cloud_file/s3.rb
+++ lib/puppet/provider/cloud_file/s3.rb
@@ -18,14 +18,14 @@ Puppet::Type.type(:cloud_file).provide(:s3, :parent => Puppet::Provider::CloudFi
 
   def configure_aws
     if resource[:access_key_id] != :undef && resource[:secret_access_key] != :undef
-      AWS.config(:access_key_id => resource[:access_key_id],
+      Aws.config(:access_key_id => resource[:access_key_id],
                  :secret_access_key => resource[:secret_access_key])
     end
   end
 
   def s3
     configure_aws
-    AWS::S3.new
+    Aws::S3::Client.new
   end
 
   def bucket_name
@@ -36,12 +36,8 @@ Puppet::Type.type(:cloud_file).provide(:s3, :parent => Puppet::Provider::CloudFi
     resource[:source].split("/")[1..-1].join("/")
   end
 
-  def bucket
-    s3.buckets[bucket_name]
-  end
-
   def object
-    bucket.objects[source_path]
+    s3.get_object(bucket:bucket_name, key:source_path)
   end
 
   def write(data)
@@ -59,15 +55,17 @@ Puppet::Type.type(:cloud_file).provide(:s3, :parent => Puppet::Provider::CloudFi
 
   def payload
     begin
-      object.read
-    rescue AWS::S3::Errors::InvalidAccessKeyId
+      object.body.read
+    rescue Aws::S3::Errors::InvalidAccessKeyId
       raise Puppet::Error, "Invalid Access Key ID"
-    rescue AWS::S3::Errors::SignatureDoesNotMatch
+    rescue Aws::S3::Errors::SignatureDoesNotMatch
       raise Puppet::Error, "Invalid Access Key ID and/or Secret Access Key"
-    rescue AWS::S3::Errors::NoSuchBucket
+    rescue Aws::S3::Errors::NoSuchBucket
       raise Puppet::Error, "Bucket Not Found (#{bucket_name})"
-    rescue AWS::S3::Errors::NoSuchKey
+    rescue Aws::S3::Errors::NoSuchKey
       raise Puppet::Error, "Remote File Not Found (#{source_path})"
+    rescue Aws::S3::Errors::AccessDenied
+      raise Puppet::Error, "Access Denied to S3 file"
     rescue => detail
       p detail
     end
